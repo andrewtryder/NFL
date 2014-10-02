5bc47ae6bd6bb049be249ffb739eb5fd3d20d4f7
spline <reticulatingspline@github.com>
2014-10-02 13:12:46 -0400

    Alias updates.

--- db/sql/nflplayeralias.sql
+++ db/sql/nflplayeralias.sql
@@ -9,3 +9,42 @@ CREATE TABLE IF NOT EXISTS `nflplayeralias` (
 
 /* NFL PLAYER ALIASES */
 INSERT INTO nflplayeralias (alias, name) VALUES ('rgknee', 'robert griffin iii');
+INSERT INTO nflplayeralias (alias, name) VALUES ('kcknee', 'kirk cousins');
+INSERT INTO nflplayeralias (alias, name) VALUES ('kc1', 'kirk cousins');
+INSERT INTO nflplayeralias (alias, name) VALUES ('rw1', 'russell wilson');
+INSERT INTO nflplayeralias (alias, name) VALUES ('dangeruss', 'russell wilson');
+INSERT INTO nflplayeralias (alias, name) VALUES ('kaeperrape', 'colin kaepernick');
+INSERT INTO nflplayeralias (alias, name) VALUES ('honey badger', 'tyrann mathieu');
+INSERT INTO nflplayeralias (alias, name) VALUES ('muscle hamster', 'doug martin');
+INSERT INTO nflplayeralias (alias, name) VALUES ('black unicorn', 'martellus bennett');
+INSERT INTO nflplayeralias (alias, name) VALUES ('switch', 'adrian peterson');
+INSERT INTO nflplayeralias (alias, name) VALUES ('AP', 'adrian peterson');
+INSERT INTO nflplayeralias (alias, name) VALUES ('Allday', 'adrian peterson');
+INSERT INTO nflplayeralias (alias, name) VALUES ('All day', 'adrian peterson');
+INSERT INTO nflplayeralias (alias, name) VALUES ('purple jesus', 'adrian peterson');
+INSERT INTO nflplayeralias (alias, name) VALUES ('elevator', 'ray rice');
+INSERT INTO nflplayeralias (alias, name) VALUES ('matty ice', 'matt ryan');
+INSERT INTO nflplayeralias (alias, name) VALUES ('mattural', 'matt ryan');
+INSERT INTO nflplayeralias (alias, name) VALUES ('beast mode', 'marshawn lynch');
+INSERT INTO nflplayeralias (alias, name) VALUES ('beastmode', 'marshawn lynch');
+INSERT INTO nflplayeralias (alias, name) VALUES ('beast', 'marshawn lynch');
+INSERT INTO nflplayeralias (alias, name) VALUES ('pocket hercules', 'maurice jones-drew');
+INSERT INTO nflplayeralias (alias, name) VALUES ('mjd', 'maurice jones-drew');
+INSERT INTO nflplayeralias (alias, name) VALUES ('run dmc', 'darren mcfadden');
+INSERT INTO nflplayeralias (alias, name) VALUES ('slot machine', 'wes welker');
+INSERT INTO nflplayeralias (alias, name) VALUES ('legatron', 'greg zuerlein');
+INSERT INTO nflplayeralias (alias, name) VALUES ('amish rifle', 'ryan fitzpatrick');
+INSERT INTO nflplayeralias (alias, name) VALUES ('anytime', 'devin hester');
+INSERT INTO nflplayeralias (alias, name) VALUES ('breesus', 'drew brees');
+INSERT INTO nflplayeralias (alias, name) VALUES ('cj2k', 'chris johnson');
+INSERT INTO nflplayeralias (alias, name) VALUES ('fitzmagic', 'ryan fitzpatrick');
+INSERT INTO nflplayeralias (alias, name) VALUES ('minitron', 'julian edelman');
+INSERT INTO nflplayeralias (alias, name) VALUES ('red rifle', 'andy dalton');
+INSERT INTO nflplayeralias (alias, name) VALUES ('sanchize', 'mark sanchez');
+INSERT INTO nflplayeralias (alias, name) VALUES ('shady', 'lesean mccoy');
+INSERT INTO nflplayeralias (alias, name) VALUES ('the sheriff', 'peyton manning');
+INSERT INTO nflplayeralias (alias, name) VALUES ('scam', 'cam newton');
+INSERT INTO nflplayeralias (alias, name) VALUES ('superman', 'cam newton');
+INSERT INTO nflplayeralias (alias, name) VALUES ('laptop', 'cam newton');
+INSERT INTO nflplayeralias (alias, name) VALUES ('tom terrific', 'tom brady');
+
